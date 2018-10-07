d63ecc898bc59e3391071a2358b94fd32f0f0139
Code6level <3246254854@qq.com>
2018-10-07 15:56:26 +0800

    搭建整体的功能架构

--- connection.h
+++ connection.h
@@ -8,9 +8,16 @@
 #include <QtXml>
 
 static bool createConnection(){
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    QSqlDatabase db;
+    if(QSqlDatabase::contains("qt_sql_default_connection"))
+        db = QSqlDatabase::database("qt_sql_default_connection");
+    else
+        db = QSqlDatabase::addDatabase("QSQLITE");
 
-    //db.setHostName("yafeilinux");
+
+    //QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+
+
     db.setDatabaseName("F:\\QtProject\\FaultDetection\\testDB.db");
 
     if (!db.open()) {
--- insertsensor.cpp
+++ insertsensor.cpp
@@ -1,5 +1,8 @@
 #include "insertsensor.h"
 #include "ui_insertsensor.h"
+#include <QSqlDatabase>
+#include  <QDebug>
+#include <QSqlQuery>
 
 insertSensor::insertSensor(QWidget *parent) :
     QDialog(parent),
@@ -16,5 +19,33 @@ insertSensor::~insertSensor()
 void insertSensor::on_pushButton_clicked()
 {
     QString insertsensor;
+    QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE","wconntosqlite");
+    mydb.setDatabaseName("f:\\QT Project\\FaultDetection\\testDB.db");
+            if(mydb.open())
+            {
+                QSqlQuery query(mydb);
+                QString sqlstr="insert into sensor (type,name,axis,frequency,lower_limit,upper_limit,resolution,scale,unit,remark) values ('"
 
+                        +ui->lineEdit->text()+"','"
+
+                        +ui->lineEdit_2->text()+"','"
+
+                        +ui->lineEdit_3->text()+"','"
+
+                        +ui->lineEdit_4->text()+"','"
+
+                        +ui->lineEdit_5->text()+"','"
+
+                        +ui->lineEdit_6->text()+"','"
+
+                        +ui->lineEdit_7->text()+"','"
+
+                        +ui->lineEdit_8->text()+"','"
+
+                        +ui->lineEdit_9->text()+"','"
+
+                        +ui->lineEdit_10->text()+"')";
+                bool isok=query.exec(sqlstr);
+                if(isok)qDebug()<<"数据写入成功";
+            }
 }
